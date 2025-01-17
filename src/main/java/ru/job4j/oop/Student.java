45e6f0da37cded8304186ac5183690a4e5fd8538
KIRILL <unisolll48@yandex.ru>
2025-01-17 13:46:33 +0300

    1.2. Вызов метода объекта [#166085]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -1,2 +1,16 @@
-package ru.job4j.oop;public class Student {
+package ru.job4j.oop;
+
+public class Student {
+    public void sing()  {
+        System.out.println("I believe I can fly");
+        System.out.println("I believe I can fly");
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.sing();
+        petya.sing();
+        petya.sing();
+    }
 }
